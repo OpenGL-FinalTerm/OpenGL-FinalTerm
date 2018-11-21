e0d523c470fa7a3c805dbe9dc6922da98f2d7e77
OnlyFall <34495911+OnlyFall@users.noreply.github.com>
2018-11-21 22:29:16 +0900

    Create Scene01_Map1.cpp

---
+++ OpenGL_Framework-master/GLFramework/Scene01_Map1.cpp
@@ -0,0 +1,54 @@
+#include "stdafx.h"
+#include "Scene01_Map1.h"
+#include "GLFramework.h"
+
+S01Map1::S01Map1()
+{
+}
+
+S01Map1::~S01Map1()
+{
+}
+
+void S01Map1::init()
+{-
+	m_Camera.setDistance(-300.f);
+	m_Camera.setPerspective(30.f, 0.125f, 7'000.f);
+	m_Camera.setSensitivity(10.f);
+}
+
+void S01Map1::exit()
+{
+}
+
+void S01Map1::reset()
+{
+}
+
+void S01Map1::render()
+{
+	m_Camera.ready();
+	glColor3f(1.0f, 1.0f, 0.0f);
+	glutWireCube(20);
+}
+
+void S01Map1::reshape(int w, int h)
+{
+}
+
+void S01Map1::keyboard(int key, bool pressed, int x, int y, bool special)
+{
+}
+
+void S01Map1::mouse(int button, bool pressed, int x, int y)
+{
+}
+
+void S01Map1::motion(bool pressed, int x, int y)
+{
+	m_Camera.rotate(x, y);
+}
+
+void S01Map1::update(float fDeltaTime)
+{
+}
